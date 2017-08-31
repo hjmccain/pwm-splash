fc556d3f367b48b5087b37aec7f4c8c8dd21171e
hjmccain <hannahjmccain@gmail.com>
2017-08-31 11:38:39 -0400

    Serve build.

--- server/app.js
+++ server/app.js
@@ -43,6 +43,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+// app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../build'));
+}
 
 module.exports = app;
